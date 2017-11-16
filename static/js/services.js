723bce167710ae1658543a0e32830be155d0ca8e
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-11-16 02:27:29 +0100

    new modifs

--- static/js/services.js
+++ static/js/services.js
@@ -2,28 +2,9 @@ angular.module('myApp').factory('getDataService', ['$q','$http', function($q,$ht
     return {
         getData: function (iri) {
             var deferred = $q.defer();
-            var url = "getResource?url="+iri;
+            var url = "/parkingloader/getResource?url="+iri;
             $http.get(url).then(function(response) {deferred.resolve(response.data);});
             return deferred.promise;
         }
     };
 }]);
-
-
-angular.module('myApp').factory('getDataService1', function($http,$q) {
-       return {
-         getData: function() {
-           var deferred = $q.defer();
-           var url = "http://127.0.0.1:5000/getResource?callback=JSON_CALLBACK&ldpr="+"http://localhost:8080/marmotta/ldp";
-  
-         $http.get(url).success(function (data, status, headers, config) {
-                console.log(data);
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                //this always gets called
-                console.log(status);
-                deferred.reject(status);
-            });
-            return deferred.promise;
-     }  }
- });
