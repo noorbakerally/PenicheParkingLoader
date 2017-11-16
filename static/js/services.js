64a7b5776fb42b898c42b56588bd8418224ed13b
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-11-16 02:30:55 +0100

    new modifs

--- static/js/services.js
+++ static/js/services.js
@@ -2,7 +2,7 @@ angular.module('myApp').factory('getDataService', ['$q','$http', function($q,$ht
     return {
         getData: function (iri) {
             var deferred = $q.defer();
-            var url = "/parkingloader/getResource?url="+iri;
+            var url = "http://opensensingcity.emse.fr/parkingloader/getResource?url="+iri;
             $http.get(url).then(function(response) {deferred.resolve(response.data);});
             return deferred.promise;
         }
