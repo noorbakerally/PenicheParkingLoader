ec539d34d2ed6065249809e44f2bab9884f49f05
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-11-16 02:30:01 +0100

    new modifs

--- static/js/services.js
+++ static/js/services.js
@@ -8,3 +8,22 @@ angular.module('myApp').factory('getDataService', ['$q','$http', function($q,$ht
         }
     };
 }]);
+
+
+angular.module('myApp').factory('getDataService1', function($http,$q) {
+       return {
+         getData: function() {
+           var deferred = $q.defer();
+           var url = "http://127.0.0.1:5000/getResource?callback=JSON_CALLBACK&ldpr="+"http://localhost:8080/marmotta/ldp";
+  
+         $http.get(url).success(function (data, status, headers, config) {
+                console.log(data);
+                deferred.resolve(data);
+            }).error(function (data, status, headers, config) {
+                //this always gets called
+                console.log(status);
+                deferred.reject(status);
+            });
+            return deferred.promise;
+     }  }
+ });
