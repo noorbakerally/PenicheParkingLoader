bd528bbf0c2980e8fc5c07b5e9264440130cad04
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-12-11 14:56:46 +0100

    server modified

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import requests
 import json
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
 from flask_cors import CORS
+import urllib2
+
 app = Flask(__name__,static_url_path='/static')
 CORS(app)
 
@@ -20,6 +22,20 @@ jinja_options.update(dict(
 ))
 app.jinja_options = jinja_options
 
+
+
+cservice = "http://rdfvalidator.mybluemix.net/validate"
+f = "application/ld+json"
+t = "text/turtle"
+headers = {"Content-Type":"application/x-www-form-urlencoded"}
+def loadData(durl):
+        data = urllib2.urlopen(durl).read()
+        data = {"content":data,"from":f,"to":t}
+        response = requests.post(cservice,data=data,headers=headers)
+        return response.text
+
+
+
 @app.route("/")
 def hello():
 	return render_template('index.html')
@@ -30,23 +46,30 @@ def getName(s):
 
 @app.route("/getResource")
 def getResource():
-	url = request.args.get('url')
-	
+	static = request.args.get('static')
+	dynamic = request.args.get('dynamic')
 
 	g = Graph ()
-	g.parse(url,format="turtle")
+	g.parse(data=loadData(static),format="turtle")
+	g.parse(data=loadData(dynamic),format="turtle")
 
 	query = """
-	PREFIX pk: <http://opensensingcity.emse.fr/ontologies/parking/>
-	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-	SELECT ?parking ?nbCarAv ?nbMotocycleAv ?nbBicycleAv ?lat ?long ?label
+	PREFIX geo: <ttp://www.w3.org/2003/01/geo/wgs84_pos#> 
+	PREFIX grepark: <http://opensensingcity.emse.fr/data/parking/grenoble/> 
+	PREFIX mv: <http://schema.mobivoc.org/> 
+	PREFIX o: <http://opensensingcity.emse.fr/ontologies/availability/>
+	PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+	PREFIX sosa: <http://www.w3.org/ns/sosa/> 
+	PREFIX xml: <http://www.w3.org/XML/1998/namespace> 
+	PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+	SELECT ?parking ?label ?totalCapacity ?availableQty ?lat ?long
 	WHERE {
-		?parking a pk:ParkingFacility .
+		?parking a mv:ParkingFacility .
 		OPTIONAL { ?parking rdfs:label  ?label . }
-		OPTIONAL { ?parking pk:nbCarParkingPlaces ?nbCarAv . }
-		OPTIONAL { ?parking pk:nbBicycleParkingPlaces ?nbBicyleAv . }
-		OPTIONAL { ?parking pk:nbMotorcycleParkingPlaces ?nbMotocycleAv . }
-		OPTIONAL { ?parking pk:nbBicycleParkingPlaces ?nbBicycleAv . }
+		OPTIONAL { ?parking mv:totalCapacity ?totalCapacity . }
+		OPTIONAL { ?parking o:availability/o:availableQuantity ?availableQty . }
 		OPTIONAL { ?parking geo:lat ?lat . }
 		OPTIONAL { ?parking geo:long ?long . }
 	} """
@@ -56,12 +79,12 @@ def getResource():
 			parking = {}
 
 			parking["iri"] = result[0]
-			parking["carAV"] = result[1]
-			parking["motoAV"] = result[2]
-			parking["bicyleAV"] = result[3]
+			parking["label"] = result[1]
+			parking["totalCapacity"] = result[2]
+			parking["availableQty"] = result[3]
 			parking["geoLat"] = float(result[4])
 			parking["geoLong"] = float(result[5])
-			parking["label"] = result[6]
+			
 
 			parkings.append(parking)
 	
